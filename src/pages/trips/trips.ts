b46399500e43c6e2d11e381b00176d09952470e3
lindseygilmore <lindseygilmore21@gmail.com>
2017-10-07 16:02:14 -0500

    trips component added

---
+++ src/pages/trips/trips.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-trips',
+  templateUrl: 'trips.html'
+})
+export class TripsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
